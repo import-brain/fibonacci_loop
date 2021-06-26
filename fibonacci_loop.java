30071b9c0a30bb4ab0db01e8b52e8549e34ffd91
ericcheng2005 <68196762+import-brain@users.noreply.github.com>
2021-06-26 12:44:07 -0400

    first 50 terms of fibonacci numbers
    first 50 terms of fibonacci numbers

---
+++ fibonacci_loop.java
@@ -0,0 +1,29 @@
+public class fibonacci_loop {
+
+    public static void main(String[] args) {
+
+        // declaring start of fibonacci numbers
+
+        long preceding_number = 1;
+
+        long oldest_preceding_number = 0;
+
+        long current_number = 1;
+
+        System.out.printf("%d%n", 1);
+
+        for (int i = 0; i < 50; i = i + 1) {
+
+            System.out.printf("%d%n", current_number);
+
+            current_number = current_number + preceding_number;
+
+            oldest_preceding_number = preceding_number;
+
+            preceding_number = current_number - oldest_preceding_number;
+
+            // math
+
+        }
+    }
+}
